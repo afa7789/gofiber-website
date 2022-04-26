87d28ff6fc25a29b40b9d86a488c1a3236c1c474
Arthur Abeilice <afa7789@gmail.com>
2022-04-26 16:10:04 -0300

    Creating Blog Post editor

--- internal/server/render_templates.go
+++ internal/server/render_templates.go
@@ -6,11 +6,32 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// Template creates a template
+// mainPage creates a mainPage template
 func (s *Server) mainPage() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		return c.Status(http.StatusOK).Render("index.html", fiber.Map{
-			"Title": "afa7789 - Computer Engineering Solutions",
+			"Title":        "afa7789 - Computer Engineering Solutions",
+			"SharedHeader": false,
 		})
+	}
+}
+
+// blogEditor opens the template
+func (s *Server) blogEditor() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		post_id := c.Params("post_id")
+		if post_id != "" {
+			return c.Status(http.StatusOK).Render("blog_editor.html", fiber.Map{
+				"Title":        "Blog Post Editor - " + post_id + " - afa7789 ",
+				"SharedHeader": true,
+				"PostID":       post_id,
+			})
+		} else {
+			return c.Status(http.StatusOK).Render("blog_editor.html", fiber.Map{
+				"Title":        "Blog Post Creator - afa7789 ",
+				"SharedHeader": true,
+			})
+		}
+
 	}
 }
