c3ef5b0c76d20bc2e6b2982f4e458ba8379dd007
Arthur Abeilice <afa7789@gmail.com>
2022-04-29 09:46:13 -0300

    Now we can connect and store posts at the database.

--- internal/domain/structs.go
+++ internal/domain/structs.go
@@ -1,16 +1,19 @@
 package domain
 
+import "time"
+
 type Flags struct {
 	Port *int
 }
 
 type Post struct {
-	ID           uint   `form:"id" json:"id"`
-	Title        string `form:"title" json:"title"`
-	Synopsis     string `form:"synopsis" json:"synopsis"`
-	Image        string `form:"image" json:"image"` // This is the image path + filename.
-	Content      string `form:"content" json:"content"`
-	RelatedPosts string `form:"related_posts" json:"related_posts"` // array of int of related posts.
+	ID           uint      `form:"id" json:"id"`
+	Title        string    `form:"title" json:"title"`
+	Synopsis     string    `form:"synopsis" json:"synopsis"`
+	Image        string    `form:"image" json:"image"` // This is the image path + filename.
+	Content      string    `form:"content" json:"content"`
+	RelatedPosts string    `form:"related_posts" json:"related_posts"` // array of int of related posts.
+	CreatedAt    time.Time `form:"created_at" json:"created_at"`
 }
 
 type Repositories struct {
