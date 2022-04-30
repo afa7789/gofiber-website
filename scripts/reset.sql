96ffe3df74391094a26d9cb845d94a5728af3607
Arthur Abeilice <afa7789@gmail.com>
2022-04-30 11:26:42 -0300

    reset db

---
+++ scripts/reset.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS gofiber_website;
+CREATE DATABASE gofiber_website;
+USE gofiber_website;
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    slug VARCHAR(255),
+    image VARCHAR(255),
+    related_posts VARCHAR(255),
+    synopsis TINYTEXT,
+    content LONGTEXT,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP
+);
